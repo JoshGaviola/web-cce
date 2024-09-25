c566302d0fa626b1920b0ef9bfe47cfec4eac13c
JoshGaviola <martin100906@gmail.com>
2024-09-25 07:16:31 -0700

    background

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div class="min-h-screen bg-gradient-to-br from-[#bf9107] to-[#fed665]">
+    <!-- Container for Nav or Header -->
+    <nav class="w-full flex items-center py-4 px-6">
+      <!-- Venti Text (Logo) -->
+      <h1 class="text-2xl font-bold text-white">Venti</h1>
+    </nav>
+  </div>
+</template>
